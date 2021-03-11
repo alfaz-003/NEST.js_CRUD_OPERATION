79a9a06261d87a684b354f80afbb2b348d12c3cb
Alfaz Vahora <65657948+alfaz-003@users.noreply.github.com>
2021-03-11 12:18:14 +0530

    Update app.module.ts
    update the DB name & pass on line 8

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { MongooseModule } from '@nestjs/mongoose' ;
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [ProductsModule,MongooseModule.forRoot('mongodb+srv://<database-name>:<password>@cluster0.ylkxn.mongodb.net/nestjs-demo?retryWrites=true&w=majority')],
